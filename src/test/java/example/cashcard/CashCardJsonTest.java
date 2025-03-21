2791a9266067886b39dd89dd6d08a4973c589868
puepi <pmandel0001@yahoo.fr>
2025-03-21 14:53:39 +0100

    implemented successfully a deserialization test method

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 
+import java.io.IOException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
@@ -13,6 +15,35 @@ public class CashCardJsonTest {
     @Autowired
     private JacksonTester<CashCard> json;
 
+    @Test
+    void cashCardSerializationTest() throws IOException {
+        CashCard cashcard=new CashCard(99L,123.45);
+        assertThat(json.write(cashcard))
+                .isStrictlyEqualToJson("expected.json");
+        assertThat(json.write(cashcard))
+                .hasJsonPathNumberValue("@.id");
+        assertThat(json.write(cashcard))
+                .extractingJsonPathNumberValue("@.id")
+                .isEqualTo(99);
+        assertThat(json.write(cashcard))
+                .hasJsonPathNumberValue("@.amount");
+        assertThat(json.write(cashcard))
+                .extractingJsonPathNumberValue("@.amount")
+                .isEqualTo(123.45);
+    }
 
+    @Test
+    void cashCardDeserializationTest() throws IOException {
+        String expected="""
+                        {
+                                "id":99,
+                                "amount":123.45
+                        }
+                        """;
+        assertThat(json.parse(expected))
+                .isEqualTo(new CashCard(99L,123.45));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
+    }
 
 }
