cd0da684498b346fcf0572e0cfb684f2b1bd42c7
puepi <pmandel0001@yahoo.fr>
2025-03-21 13:04:10 +0100

    added a cashcard record in the src/main/java and implemented a test method

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -1,14 +1,18 @@
 package example.cashcard;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@JsonTest
 public class CashCardJsonTest {
 
-    @Test
-    void myFirstTest(){
-        assertThat(42).isEqualTo(42);
-    }
+    @Autowired
+    private JacksonTester<CashCard> json;
+
+
 
 }
