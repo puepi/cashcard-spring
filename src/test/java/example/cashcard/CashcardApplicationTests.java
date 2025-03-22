213ac7e7205c05a2f69bfc35412637c71e432855
puepi <pmandel0001@yahoo.fr>
2025-03-22 02:52:05 +0100

    added sql schemas and data in resources

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -32,6 +32,15 @@ class CashcardApplicationTests {
 	}
 
 	@Test
+	void shouldNotReturnACashCardWithAnUnknownId(){
+		ResponseEntity<String> response=restTemplate.getForEntity("/cashcards/1000" +
+				"",String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
+	}
+
+	@Test
 	void contextLoads() {
 	}
 
