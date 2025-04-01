91b364111741d383061bcb8442fcaa1d8f1c158e
puepi <pmandel0001@yahoo.fr>
2025-04-01 02:48:07 +0100

    added new features

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -61,10 +61,14 @@ public class CashCardController {
     } // <1>
 
     @PutMapping("/{requestedId}")
-    private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId,@RequestBody CashCard cashCardUpdate, Principal principal){
-        CashCard cashCard=cashCardRepository.findByIdAndOwner(requestedId,principal.getName());
-        CashCard updatedCashCard=new CashCard(cashCard.id(),cashCardUpdate.amount(),principal.getName());
-        cashCardRepository.save(updatedCashCard);
-        return ResponseEntity.noContent().build();
+    private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate, Principal principal){
+        CashCard cashCard=cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
+        if(cashCard!=null){
+            CashCard updatedCashCard=new CashCard(cashCard.id(),cashCardUpdate.amount(),principal.getName());
+            cashCardRepository.save(updatedCashCard);
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -166,6 +166,18 @@ class CashcardApplicationTests {
 	 }
 
 	@Test
+	void shouldNotUpdateACashCardThatIsOwnedBySomeoneElse() {
+		CashCard kumarsCard = new CashCard(null, 333.33, null);
+		HttpEntity<CashCard> request = new HttpEntity<>(kumarsCard);
+		ResponseEntity<Void> response = restTemplate
+				.withBasicAuth("sarah1", "abc123")
+				.exchange("/cashcards/102", HttpMethod.PUT, request, Void.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
+
+
+
+	@Test
 	void contextLoads() {
 	}
 
